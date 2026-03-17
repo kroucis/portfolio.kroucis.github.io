5ea979b46a14ce8a571c9edac00ad7afd7fa8050
Grimless <kyle@kyleroucis.com>
2026-03-17 00:11:11 -0400

    Add Project info and images

--- src/Projects.elm
+++ src/Projects.elm
@@ -16,14 +16,40 @@ type alias Project =
 
 all : List Project
 all =
-    [ { slug = "your-project"
-      , title = "Your Project"
-      , year = "2024"
-      , image = ""
-      , summary = "A short description of what this project does."
-      , description = "A longer description. What problem does it solve? What did you learn? What's interesting about it?"
-      , tags = [ "Elm", "Web" ]
+    [ { slug = "ch3ss-m4tch"
+      , title = "Ch3ss M4tch - Game - macOS & iOS & Android"
+      , year = "2023"
+      , image = "/images/Ch3ssM4tch.png"
+      , summary = "Competitive match-3 game based loosely on chess. First Clockwork-based game."
+      , description = "<TODO>"
+      , tags = [ "iOS", "Android", "macOS", "Swift", "Clockwork Game Engine", "App Store", "Google Play" ]
       , github = Nothing
-      , urls = []
+      , urls =
+            [ ( "App Store", "https://apps.apple.com/tr/app/ch3ss-m4tch/id1604265608" )
+            ]
+      }
+    , { slug = "simple-swaps"
+      , title = "Simple Swaps - Game - iOS & Android"
+      , year = "2024"
+      , image = "/images/SimpleSwaps.jpg"
+      , summary = "Block swapping and pattern matching game focused on engine features and simplicity."
+      , description = "<TODO>"
+      , tags = [ "iOS", "Android", "Swift", "Clockwork Game Engine", "App Store", "Google Play" ]
+      , github = Nothing
+      , urls =
+            [ ( "App Store", "https://apps.apple.com/tr/app/ch3ss-m4tch/id1604265608" )
+            ]
+      }
+    , { slug = "pixel-forge"
+      , title = "Pixel Forge - Graphics Tool - macOS"
+      , year = "2024"
+      , image = "/images/PixelForge.png"
+      , summary = "Metal-based shader creation tool."
+      , description = "<TODO>"
+      , tags = [ "macOS", "Swift", "Metal", "App Store" ]
+      , github = Nothing
+      , urls =
+            [ ( "App Store", "https://apps.apple.com/us/app/pixel-forge/id6473090142?mt=12" )
+            ]
       }
     ]
